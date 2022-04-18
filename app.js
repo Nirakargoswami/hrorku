d2fd0a1bc6f0c1764be0d06e27fa7853c217d425
nirakar <goswaminirakar65.com>
2022-04-18 16:20:50 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,153 @@
+const express = require("express");
+const Joi = require("joi");
+
+
+
+
+
+const app = express();
+app.use(express.json());
+const courses = [
+    { id : '1', name : "course1"},
+    { id : '2', name : "course2"},
+    { id : '3', name : "course3"}
+]
+
+app.get("/", (req,res) => {
+    res.send("helloo world");
+});
+app.get("/api/courses", (req,res) => {
+    res.send(courses);
+});
+
+
+app.post("/api/courses" , (req,res) => {
+
+
+
+const result =   validation(req.body); 
+console.log(result);
+if(result.error){
+    res.status(400).send(result.error);
+    return;
+}
+  
+    console.log("post  is wokring ")
+    const course = {
+        id : courses.length + 1,
+        name : req.body.name 
+    }
+ courses.push(course);
+ console.log(courses)
+ res.send(course);
+} )
+
+
+app.put("/api/courses/:id", (req,res) => {
+    const id = req.params.id;
+    const course = courses.find( x => {
+        return  x.id == id;
+     });
+     
+     
+     if(!course)res.status(404).send("course is not found");
+
+
+     
+    
+    const result =   validation(req.body); 
+    console.log(result);
+    if(result.error){
+        res.status(400).send(result.error);
+        return;
+    }
+    
+course.name = req.body.name;
+res.send(course)
+
+})
+
+const validation = (courseobj) => {
+    const schema = Joi.object(  {
+        name : Joi.string().min(3).required()
+    });
+    
+    return  schema.validate(courseobj)
+
+}
+
+
+
+
+
+app.get("/api/courses/:id", (req,res) => {
+const id = req.params.id;
+
+const course = courses.find( x => {
+   return  x.id == id;
+});
+console.log(course);
+
+course ?   res.send(course) : res.status(404).send("course is not found"); 
+
+//convertd whole mathod into the function 
+findcourse(req,res );
+ })
+
+app.delete('/api/courses/:id', (req, res) => {
+    const id = req.params.id;
+
+const course = courses.find( x => {
+   return  x.id == id;
+});
+if(!course)res.status(404).send("course is not found");
+
+const index = courses.indexOf(course) ;
+
+courses.splice(index,1);
+res.send(course);
+
+ 
+})
+
+function findcourse(req,res){
+    const id = req.params.id;
+
+    const course = courses.find( x => {
+       return  x.id == id;
+    });
+    console.log(course);
+    
+    course ? res.send(course) : res.status(404).send("course is not found");  
+    
+     
+        
+}
+
+
+
+
+const port = process.env.PORT || 3000;
+
+app.listen( port , () => {
+    console.log(`working on port ${port}....`);
+});
+
+
+
+
+
+
+// const express = require("express");
+// const app = express();
+// app.get("/", (req,res) => {
+//     res.send("helloo world");
+// });
+
+
+
+// const port = process.env.PORT || 3000;
+// app.listen( port , () => {
+//     console.log(`working on port ${port}....`);
+// });
+
---
+++ test.js
@@ -0,0 +1,79 @@
+// // const { x } = require("joi");
+
+const { compile } = require("joi");
+
+// // const Same  = (arr1,arr2 ) => {
+// //   var tool  = {
+
+// //   };
+// //   arr1.map( x => {
+// //     tool[x] = 1;
+// //   });
+// //   console.log(tool);
+
+
+// // }
+// // Same([1,2,3]);
+
+
+
+
+// class Student {
+//   constructor (fitsrnaem , lastname,mark){
+
+//     this.firstnaem = fitsrnaem;
+//     this.lastname = lastname;
+//   this.mrak = [mark];
+//   this.tides = 0
+//   }
+// fullname (){
+//   return `your full name is ${this.firstnaem}  ${this.lastname}`
+
+// }
+// marklate(){
+//   this.tides +=1;
+//   if( this.tides >= 3){
+//     return `u are expelet from scholl ${this.tides}`
+//   }
+// }
+// addmrak(marks){
+
+// this.mrak.push(marks);
+// console.log("runkhj")
+// return this.mrak
+// }
+// average ()
+// {
+// let average = this.mrak.reduce( (a,b) => {
+//   return a + b
+// });
+// return average / this.mrak.length;
+
+
+// }
+// }
+
+
+// const Nitin = new Student( "vikas","singh",35);
+
+// console.log(Nitin.marklate());
+// console.log(Nitin.marklate());
+// console.log(Nitin.marklate());
+// console.log(Nitin.marklate());
+
+
+
+class Point {
+  constructor(x,y ){
+    this.x = x ;
+    this.y = y;
+  }
+static distance(a,b){
+  const dx = a.x - b.x;
+  console.log(a.x,b.x)
+  console.log(dx);
+}
+}
+const p1 = new Point( 8,9);
+const p2 = new Point( 7,10);
+console.log(Point.distance(p1,p2));
