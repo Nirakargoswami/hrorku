910b58c3b27baf7747fe7cb8f686afcdce156ece
nirakar <goswaminirakar65.com>
2022-04-19 10:06:24 +0530

    Procfile

--- app.js
+++ app.js
@@ -1,153 +1,153 @@
-const express = require("express");
-const Joi = require("joi");
+//const express = require("express");
+//const Joi = require("joi");
 
 
 
 
 
-const app = express();
-app.use(express.json());
-const courses = [
-    { id : '1', name : "course1"},
-    { id : '2', name : "course2"},
-    { id : '3', name : "course3"}
-]
+// const app = express();
+// app.use(express.json());
+// const courses = [
+//     { id : '1', name : "course1"},
+//     { id : '2', name : "course2"},
+//     { id : '3', name : "course3"}
+// ]
 
-app.get("/", (req,res) => {
-    res.send("helloo world");
-});
-app.get("/api/courses", (req,res) => {
-    res.send(courses);
-});
-
-
-app.post("/api/courses" , (req,res) => {
+// app.get("/", (req,res) => {
+//     res.send("helloo world");
+// });
+// app.get("/api/courses", (req,res) => {
+//     res.send(courses);
+// });
 
 
+// app.post("/api/courses" , (req,res) => {
 
-const result =   validation(req.body); 
-console.log(result);
-if(result.error){
-    res.status(400).send(result.error);
-    return;
-}
+
+
+// const result =   validation(req.body); 
+// console.log(result);
+// if(result.error){
+//     res.status(400).send(result.error);
+//     return;
+// }
   
-    console.log("post  is wokring ")
-    const course = {
-        id : courses.length + 1,
-        name : req.body.name 
-    }
- courses.push(course);
- console.log(courses)
- res.send(course);
-} )
+//     console.log("post  is wokring ")
+//     const course = {
+//         id : courses.length + 1,
+//         name : req.body.name 
+//     }
+//  courses.push(course);
+//  console.log(courses)
+//  res.send(course);
+// } )
 
 
-app.put("/api/courses/:id", (req,res) => {
-    const id = req.params.id;
-    const course = courses.find( x => {
-        return  x.id == id;
-     });
+// app.put("/api/courses/:id", (req,res) => {
+//     const id = req.params.id;
+//     const course = courses.find( x => {
+//         return  x.id == id;
+//      });
      
      
-     if(!course)res.status(404).send("course is not found");
+//      if(!course)res.status(404).send("course is not found");
 
 
      
     
-    const result =   validation(req.body); 
-    console.log(result);
-    if(result.error){
-        res.status(400).send(result.error);
-        return;
-    }
+//     const result =   validation(req.body); 
+//     console.log(result);
+//     if(result.error){
+//         res.status(400).send(result.error);
+//         return;
+//     }
     
-course.name = req.body.name;
-res.send(course)
+// course.name = req.body.name;
+// res.send(course)
 
-})
+// })
 
-const validation = (courseobj) => {
-    const schema = Joi.object(  {
-        name : Joi.string().min(3).required()
-    });
+// const validation = (courseobj) => {
+//     const schema = Joi.object(  {
+//         name : Joi.string().min(3).required()
+//     });
     
-    return  schema.validate(courseobj)
+//     return  schema.validate(courseobj)
 
-}
-
-
+// }
 
 
 
-app.get("/api/courses/:id", (req,res) => {
-const id = req.params.id;
 
-const course = courses.find( x => {
-   return  x.id == id;
-});
-console.log(course);
 
-course ?   res.send(course) : res.status(404).send("course is not found"); 
+// app.get("/api/courses/:id", (req,res) => {
+// const id = req.params.id;
 
-//convertd whole mathod into the function 
-findcourse(req,res );
- })
+// const course = courses.find( x => {
+//    return  x.id == id;
+// });
+// console.log(course);
 
-app.delete('/api/courses/:id', (req, res) => {
-    const id = req.params.id;
+// course ?   res.send(course) : res.status(404).send("course is not found"); 
 
-const course = courses.find( x => {
-   return  x.id == id;
-});
-if(!course)res.status(404).send("course is not found");
+// //convertd whole mathod into the function 
+// findcourse(req,res );
+//  })
 
-const index = courses.indexOf(course) ;
+// app.delete('/api/courses/:id', (req, res) => {
+//     const id = req.params.id;
 
-courses.splice(index,1);
-res.send(course);
+// const course = courses.find( x => {
+//    return  x.id == id;
+// });
+// if(!course)res.status(404).send("course is not found");
+
+// const index = courses.indexOf(course) ;
+
+// courses.splice(index,1);
+// res.send(course);
 
  
-})
+// })
 
-function findcourse(req,res){
-    const id = req.params.id;
+// function findcourse(req,res){
+//     const id = req.params.id;
 
-    const course = courses.find( x => {
-       return  x.id == id;
-    });
-    console.log(course);
+//     const course = courses.find( x => {
+//        return  x.id == id;
+//     });
+//     console.log(course);
     
-    course ? res.send(course) : res.status(404).send("course is not found");  
+//     course ? res.send(course) : res.status(404).send("course is not found");  
     
      
         
-}
+// }
 
+
+
+
+// const port = process.env.PORT || 5000;
+
+// app.listen( port , () => {
+//     console.log(`working on port ${port}....`);
+// });
+
+
+
+
+
+
+const express = require("express");
+const app = express();
+app.get("/", (req,res) => {
+    res.send("helloo world");
+});
 
 
 
 const port = process.env.PORT || 3000;
-
 app.listen( port , () => {
     console.log(`working on port ${port}....`);
 });
-
-
-
-
-
-
-// const express = require("express");
-// const app = express();
-// app.get("/", (req,res) => {
-//     res.send("helloo world");
-// });
-
-
-
-// const port = process.env.PORT || 3000;
-// app.listen( port , () => {
-//     console.log(`working on port ${port}....`);
-// });
 
