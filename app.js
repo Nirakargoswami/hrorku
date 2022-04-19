dac66a2d1f965dec5549654a53fd8c47d41dbeb6
nirakar <goswaminirakar65.com>
2022-04-19 10:58:41 +0530

    commit

--- app.js
+++ app.js
@@ -140,13 +140,14 @@
 
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
 app.get("/", (req,res) => {
     res.send("helloo world");
 });
 
 
 
-const port = process.env.PORT || 3000;
+
 app.listen( port , () => {
     console.log(`working on port ${port}....`);
 });
